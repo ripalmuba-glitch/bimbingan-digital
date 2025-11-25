05763dd6995069b92b8fc8308209b437a27981fa
Rivaldianto <ripalmuba@gmail.com>
2025-11-25 13:48:08 +0700

    Fix: Paksa upload file CSS/JS build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA TAMBAHKAN INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // PAKSA HTTPS SAAT PRODUCTION
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
